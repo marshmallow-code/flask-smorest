3816e60997ecc852e4c31fea781edf4189a0cc25
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2017-02-27 18:13:47 +0100

    Minor setup.py rework

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
