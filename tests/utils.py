67780196495892198bf8d14e3d47bd3044db048e
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2017-03-01 17:08:07 +0100

    Minor pylint fix

--- tests/utils.py
+++ tests/utils.py
@@ -4,7 +4,6 @@ from werkzeug.utils import cached_property
 from flask import Response
 
 
-# pylint: disable=too-many-ancestors
 class JSONResponse(Response):
     """
     A Response class with extra useful helpers, i.e. ``.json`` property.
@@ -12,6 +11,7 @@ class JSONResponse(Response):
     Taken from https://github.com/frol/flask-restplus-server-example/
     Thanks Vlad Frolov
     """
+    # pylint: disable=too-many-ancestors
 
     @cached_property
     def json(self):
