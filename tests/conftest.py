c1b86c0446397c17c1b695ec4c7a5c4b812235d1
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2018-02-16 17:52:28 +0100

    Add CounterSchema

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 
 import pytest
 
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load, post_dump
 
 from flask import Flask
 
@@ -37,16 +37,39 @@ def app(request):
     return _app
 
 
+class CounterSchema(Schema):
+    """Base Schema with load/dump counters"""
+
+    load_count = 0
+    dump_count = 0
+
+    @classmethod
+    def reset_load_count(cls):
+        cls.load_count = 0
+
+    @classmethod
+    def reset_dump_count(cls):
+        cls.dump_count = 0
+
+    @post_load
+    def increment_load_count(self, _):
+        self.__class__.load_count += 1
+
+    @post_dump
+    def increment_dump_count(self, _):
+        self.__class__.dump_count += 1
+
+
 @pytest.fixture
 def schemas():
 
-    class DocSchema(Schema):
+    class DocSchema(CounterSchema):
         class Meta:
             strict = True
         item_id = fields.Int(dump_only=True)
         field = fields.Int(attribute='db_field')
 
-    class DocEtagSchema(Schema):
+    class DocEtagSchema(CounterSchema):
         class Meta:
             strict = True
         field = fields.Int(attribute='db_field')
