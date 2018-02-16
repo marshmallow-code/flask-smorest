c513d97e3a9ad7b92e6239ec26583e4502ba9617
Jérôme Lafrechoux <jlafrechoux@nobatek.com>
2018-02-16 21:39:02 +0100

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -8,32 +8,16 @@ with open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='flask-rest-api',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version='0.1.0',
-
     description='Build a REST API with Flask',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/Nobatek/flask-rest-api',
-
-    # Author details
     author='Jérôme Lafréchoux',
     author_email='jlafrechoux@nobatek.com',
-
-    # Choose your license
     license='MIT',
-
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Internet :: WWW/HTTP',
         'Environment :: Web Environment',
@@ -43,24 +27,12 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-
-    # What does your project relate to?
     keywords='flask REST api',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     package_data={
         '': ['spec/templates/*'],
     },
-
-    # List run-time dependencies here. These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    # https://caremad.io/2013/07/setup-vs-requirement/
     install_requires=[
         'werkzeug>=0.11',
         'flask>=0.11',
