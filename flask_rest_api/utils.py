50afe3b254f4773dde3c9643b8b8ea15bd6f97c6
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2018-08-21 10:05:25 +0200

    Add utils.load_info_from_docstring

--- flask_rest_api/utils.py
+++ flask_rest_api/utils.py
@@ -3,6 +3,7 @@
 from collections import defaultdict
 
 from flask import _app_ctx_stack
+from apispec.utils import trim_docstring, dedent
 
 
 # http://stackoverflow.com/a/8310229/4653485
@@ -28,3 +29,35 @@ def get_appcontext():
     if not hasattr(ctx, 'flask_rest_api'):
         ctx.flask_rest_api = defaultdict(dict)
     return ctx.flask_rest_api
+
+
+def load_info_from_docstring(docstring):
+    """Load summary and description from docstring"""
+    split_lines = trim_docstring(docstring).split('\n')
+
+    # Info is separated from rest of docstring by a '---' line
+    for index, line in enumerate(split_lines):
+        if line.lstrip().startswith('---'):
+            cut_at = index
+            break
+    else:
+        cut_at = index + 1
+
+    split_info_lines = split_lines[:cut_at]
+
+    # Description is separated from summary by an empty line
+    for index, line in enumerate(split_info_lines):
+        if line.strip() == '':
+            summary_lines = split_info_lines[:index]
+            description_lines = split_info_lines[index + 1:]
+            break
+    else:
+        summary_lines = split_info_lines
+        description_lines = []
+
+    info = {}
+    if summary_lines:
+        info['summary'] = dedent('\n'.join(summary_lines))
+    if description_lines:
+        info['description'] = dedent('\n'.join(description_lines))
+    return info
