589ed2490c69f486ac1705f1dfe5959c3d9b71b3
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2018-10-24 15:48:40 +0200

    Bump version: 0.9.2 → 0.10.0

--- flask_rest_api/__init__.py
+++ flask_rest_api/__init__.py
@@ -9,7 +9,7 @@ from .pagination import Page  # noqa
 from .error_handler import ErrorHandlerMixin
 
 
-__version__ = '0.9.2'
+__version__ = '0.10.0'
 
 
 class Api(DocBlueprintMixin, ErrorHandlerMixin):
