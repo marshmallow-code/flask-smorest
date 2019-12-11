a3625eba4937c625b1418009282900869165d517
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2019-12-11 16:01:13 +0100

    tests.utils: add get_responses (copied from apispec)

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,18 @@
 from apispec.utils import build_reference
 
 
+# Getter functions are copied from apispec tests
+
 def get_schemas(spec):
     if spec.openapi_version.major < 3:
         return spec.to_dict()["definitions"]
     return spec.to_dict()["components"]["schemas"]
+
+
+def get_responses(spec):
+    if spec.openapi_version.major < 3:
+        return spec.to_dict()["responses"]
+    return spec.to_dict()["components"]["responses"]
 
 
 def build_ref(spec, component_type, obj):
