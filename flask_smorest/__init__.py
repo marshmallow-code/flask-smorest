4b27aecf52b105657b0c8d4632712c642ec9ce7e
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2020-06-19 22:27:14 +0200

    Bump version: 0.21.2 → 0.22.0

--- flask_smorest/__init__.py
+++ flask_smorest/__init__.py
@@ -7,7 +7,7 @@ from .blueprint import Blueprint  # noqa
 from .pagination import Page  # noqa
 from .error_handler import ErrorHandlerMixin
 
-__version__ = '0.21.2'
+__version__ = '0.22.0'
 
 
 class Api(APISpecMixin, ErrorHandlerMixin):
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='flask-smorest',
-    version='0.21.2',
+    version='0.22.0',
     description='Flask/Marshmallow-based REST API framework',
     long_description=long_description,
     url='https://github.com/marshmallow-code/flask-smorest',
