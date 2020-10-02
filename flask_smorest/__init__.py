e06325d7a201af6d07caa2e8e103fba15eb4b9b7
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2020-10-02 14:30:07 +0200

    Bump version: 0.24.1 → 0.25.0

--- flask_smorest/__init__.py
+++ flask_smorest/__init__.py
@@ -7,7 +7,7 @@ from .blueprint import Blueprint  # noqa
 from .pagination import Page  # noqa
 from .error_handler import ErrorHandlerMixin
 
-__version__ = '0.24.1'
+__version__ = '0.25.0'
 
 
 class Api(APISpecMixin, ErrorHandlerMixin):
