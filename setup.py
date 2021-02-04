c8f69d4b62cdca7dcb07a88853d62ec763c14f46
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2021-02-04 23:46:14 +0100

    Bump version: 0.27.0 → 0.28.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='flask-smorest',
-    version='0.27.0',
+    version='0.28.0',
     description='Flask/Marshmallow-based REST API framework',
     long_description=long_description,
     url='https://github.com/marshmallow-code/flask-smorest',
