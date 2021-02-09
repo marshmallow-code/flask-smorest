52ad32986fafec82a609f0b9e03611caa865abb5
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2021-02-09 17:07:42 +0100

    Test werkzeug.Response, not BaseResponse

--- flask_smorest/response.py
+++ flask_smorest/response.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 from functools import wraps
 import http
 
-from werkzeug.wrappers import BaseResponse
+from werkzeug import Response
 from flask import jsonify
 
 from .utils import (
@@ -77,8 +77,8 @@ class ResponseMixin:
                 result_raw, status, headers = unpack_tuple_response(
                     func(*args, **kwargs))
 
-                # If return value is a werkzeug BaseResponse, return it
-                if isinstance(result_raw, BaseResponse):
+                # If return value is a werkzeug Response, return it
+                if isinstance(result_raw, Response):
                     set_status_and_headers_in_response(
                         result_raw, status, headers)
                     return result_raw
