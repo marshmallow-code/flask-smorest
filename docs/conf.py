42853617313989a23fe7869a88d7be005f00bbfb
Jérôme Lafréchoux <jerome@jolimont.fr>
2022-01-23 21:23:55 +0100

    Update copyright 2021->2022

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import flask_smorest
 
 project = "flask-smorest"
-copyright = "2021, Nobatek/INEF4 and contributors"
+copyright = "2022, Nobatek/INEF4 and contributors"
 
 version = release = flask_smorest.__version__
 
