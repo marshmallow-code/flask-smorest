0b2a9e4077c3a375e271bf7549b4bb9ac9bf9212
Arthur Van Duynhoven <arthur.vanduynhoven@irdeto.com>
2023-01-17 17:02:23 +0100

    Issue-444: Prevent Upload file converter from overridding existing ref changes

--- flask_smorest/spec/field_converters.py
+++ flask_smorest/spec/field_converters.py
@@ -2,9 +2,8 @@
 from flask_smorest.fields import Upload
 
 
-def uploadfield2properties(self, field, **kwargs):
+def uploadfield2properties(self, field, ret, **kwargs):
     """Document Upload field properties in the API spec"""
-    ret = {}
     if isinstance(field, Upload):
         if self.openapi_version.major < 3:
             ret["type"] = "file"
