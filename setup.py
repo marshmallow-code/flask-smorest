05b0e9a6901f818d5b983e9d37f0c033f76af057
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2023-08-17 17:52:52 +0200

    Require Flask>=2.0.1
    Flask 2.0.0 support was broken in 0.42.0 but setup.py was not updated

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     python_requires=">=3.8",
     install_requires=[
         "werkzeug>=2.0.1,<3",
-        "flask>=2.0,<3",
+        "flask>=2.0.1,<3",
         "marshmallow>=3.18.0,<4",
         "webargs>=8.0.0,<9",
         "apispec[marshmallow]>=6.0.0,<7",
